9a7591156357f299be26845fc348fc29e3eae2b1
Md. Sadikur Rahman <51820432+Sadik182@users.noreply.github.com>
2025-11-09 09:55:27 +1100

    Added Bottom Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
         <title>Amazon Clone</title>
       </Head>
       <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        {/* Product Feed */}
+      </main>
     </div>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -54,7 +54,21 @@ function Header() {
       </div>
 
       {/* Bottom Navbar */}
-      <div></div>
+      <div className="flex items-center bg-amazon-light text-white text-sm space-x-3 p-2 pl-6">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today&apos;s Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   );
 }
