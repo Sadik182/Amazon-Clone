d8ca1aa38c95fd730bf156a1af17e18a6842fe97
Md. Sadikur Rahman <51820432+Sadik182@users.noreply.github.com>
2025-11-09 09:43:06 +1100

    Create heade with top navbar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,60 @@
 import React from "react";
+import Image from "next/image";
+import {
+  MagnifyingGlassIcon as SearchIcon,
+  ShoppingCartIcon,
+  Bars3Icon as MenuIcon,
+} from "@heroicons/react/24/outline";
 
 function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      {/* Top Navbar */}
+      <div className="flex items-center bg-amazon-blue px-4 py-2 grow space-x-2">
+        <div className="mt-2 flex items-center grow sm:grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            alt="logo"
+            className="cursor-pointer"
+            objectFit="contain"
+          />
+        </div>
+        {/* Search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md grow cursor-pointer bg-yellow-400 hover:bg-yellow-500 transition-all duration-300 mt-[-8px]">
+          <input
+            className="p-2 h-full w-6 grow shrink rounded-l-md focus:outline-none bg-white"
+            type="text"
+            placeholder="Search"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello, Md Sadikur Rahman</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p className="font-extrabold md:text-sm">Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="link relative flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-center text-black font-bold">
+              4
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom Navbar */}
+      <div></div>
     </header>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["links.papareact.com", "fakestoreapi.com"],
+  },
 };
 
 export default nextConfig;
