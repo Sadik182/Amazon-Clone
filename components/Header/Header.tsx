37ee7c37411774243655bb2840b7031cf7120ac6
Md. Sadikur Rahman <51820432+Sadik182@users.noreply.github.com>
2025-11-23 23:02:26 +1100

    added firebase authentication

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import {
@@ -5,8 +6,10 @@ import {
   ShoppingCartIcon,
   Bars3Icon as MenuIcon,
 } from "@heroicons/react/24/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
+  const { data: session } = useSession();
   return (
     <header>
       {/* Top Navbar */}
@@ -33,8 +36,11 @@ function Header() {
 
         {/* Right */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
-            <p>Hello, Md Sadikur Rahman</p>
+          <div
+            onClick={() => (session ? signOut() : signIn())}
+            className="link cursor-pointer"
+          >
+            <p>Hello, {session?.user?.name || "Guest"}</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
